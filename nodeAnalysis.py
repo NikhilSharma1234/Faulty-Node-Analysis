f7eb4c0fbdbb603c629bf0896d41145fa3856da4
Nikhil <nikhil342015@gmail.com>
2022-11-29 13:55:10 -0800

    Adding and removing links

--- nodeAnalysis.py
+++ nodeAnalysis.py
@@ -6,6 +6,29 @@ def main():
     # Create a new matrix object
     newMatrix = Matrix(3)
 
+    # Print empty matrix
+    print(newMatrix)
+    
+    # Adding values to the matrix (creating links between nodes)
+    try:
+        newMatrix.add(0, 2, 5)
+        newMatrix.add(1, 2, 7)
+        newMatrix.add(2, 2, 2)
+        newMatrix.add(1, 1, 6)
+    except:
+        print("Invalid position entered for adding to matrix")
+        return
+
+    # Print newMatrix
+    print(newMatrix)
+
+    # Remove values from the matrix
+    try:
+        newMatrix.remove(1, 1)
+    except:
+        print("Invalid position entered for adding to matrix")
+        return
+
     # Print newMatrix
     print(newMatrix)
 
@@ -36,6 +59,14 @@ class Matrix:
         # In each iteration, add an empty list to the main list
             newMatrix.append(newList)
         return newMatrix
+
+    # Add weight to links
+    def add(self, row, col, value):
+        self.matrix[row][col] = value
+
+    # Remove weight from links
+    def remove(self, row, col):
+        self.matrix[row][col] = 0
 
 # Ensures that the main() function is ran before anything
 if __name__ == "__main__":
